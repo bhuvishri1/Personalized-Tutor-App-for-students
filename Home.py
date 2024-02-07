625cfe4bbc9ef004c6314a635080780cd0b3cfa6
Bhuvaneswari.R <107169252+bhuvishri1@users.noreply.github.com>
2024-02-07 13:29:17 +0530

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+st.title("Personalized Tutor")
+st.markdown("This app suggest you the ways you can excel in your Academics, Get your personalized feedback, Study plan ,Question and Answer from the pdf you provide with a single click.")
+st.markdown("""
+    Welcome to **Personalized Tutor**, a comprehensive platform designed to enhance your learning and interaction experience. Explore the various features offered across three engaging apps:
+    """)
+
+st.subheader("1. Custom Feedback Generator: Chat with PDF Documents")
+st.markdown("""
+    Dive into the world of interactive document exploration! Upload your PDF files and engage in conversations with them using the innovative PDF Chatbot. Extract insights, ask questions, and uncover the power of document understanding.
+    """)
+
+st.subheader("2. Study Planner: Generating Study plan")
+st.markdown("""
+    Plan your studies more effectively with the Study Plan Generator. Receive personalized study plans based on your preferences, goals, and available time. Achieve optimal productivity and success on your learning journey.
+    """)
+
+st.subheader("3. DocuChat: Question Answering (PDF Chatbot)")
+st.markdown("""
+    Unleash the potential of question answering with the PDF Chatbot. Upload your PDF documents, ask questions, and receive instant responses. Seamlessly integrate information retrieval and interactive discussions into your study routine.
+    """)
+
+st.subheader("Get Started")
+st.markdown("""
+    Choose the app that suits your needs, and embark on a journey of interactive learning and productivity. Your exploration begins now!
+    """)
